2d26dabb1f4ac7e5bd16f5dd1a72762626231664
furkanadiiguzel <furkan.adiguzel@ug.bilkent.edu.tr>
2023-05-02 18:58:48 +0300

    commit

--- graph/model/models_gen.go
+++ graph/model/models_gen.go
@@ -2,27 +2,3 @@
 
 package model
 
-type CreateJobListingInput struct {
-	Title       string `json:"title"`
-	Description string `json:"description"`
-	Company     string `json:"company"`
-	URL         string `json:"url"`
-}
-
-type DeleteJobResponse struct {
-	DeletedJobID string `json:"deletedJobId"`
-}
-
-type JobListing struct {
-	ID          string `json:"_id"`
-	Title       string `json:"title"`
-	Description string `json:"description"`
-	Company     string `json:"company"`
-	URL         string `json:"url"`
-}
-
-type UpdateJobListingInput struct {
-	Title       *string `json:"title"`
-	Description *string `json:"description"`
-	URL         *string `json:"url"`
-}
