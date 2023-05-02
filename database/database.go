1014a1b58f8fd68470885003e5b3879a5f3fe9ea
furkanadiiguzel <furkan.adiguzel@ug.bilkent.edu.tr>
2023-05-02 20:56:12 +0300

    schema created

--- database/database.go
+++ database/database.go
@@ -1,1 +1,10 @@
 package database
+
+
+ var connectionString = "postgres://username:password@hostname:port/dbname?sslmode=require"
+	db, err := sql.Open("postgres", connectionString)
+	if err != nil {
+		fmt.Println("Error opening database connection:", err)
+		return
+	}
+	defer db.Close()
