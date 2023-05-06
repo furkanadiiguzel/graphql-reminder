d740af3dc0ad6155279d4bc1e410ec4e20163795
furkanadiiguzel <furkan.adiguzel@ug.bilkent.edu.tr>
2023-05-06 15:05:28 +0300

    db commit

--- database/database.go
+++ database/database.go
@@ -4,21 +4,47 @@ import (
 	"context"
 	"database/sql"
 	"time"
+
+	"github.com/furkanadiiguzel/graphql-reminder/graph/model"
 )
 
 var connectionString = "postgres://postgres:1234@localhost:5432/dbname?sslmode=require"
 
-func Connect() *sql.DB {
-	db, err := sql.Open("postgres", connectionString)
+type DB struct {
+	client *sql.DB
+}
+
+func Connect() *DB {
+	client, err := sql.Open("postgres", connectionString)
 	if err != nil {
 		panic(err)
 	}
 	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 	defer cancel()
-	err = db.PingContext(ctx)
+	err = client.PingContext(ctx)
 	if err != nil {
 		panic(err)
 	}
 
-	return db
+	return &DB{client: client}
+}
+func (db *DB) getReminder(id string) *model.ReminderListing {
+	var reminderListing model.ReminderListing
+	return &reminderListing
+}
+func (db *DB) getReminders() []*model.ReminderListing {
+	var reminderListings []*model.ReminderListing
+	return reminderListings
+}
+func (db *DB) CreateReminderListing(reminderInfo model.CreateReminderListingInput) *model.ReminderListing {
+	var returnReminderListing model.ReminderListing
+	return &returnReminderListing
+}
+func (db *DB) UpdateReminderListing(id string, reminderInfo model.UpdateReminderListingInput) *model.ReminderListing {
+	var ReminderListing model.ReminderListing
+	return &ReminderListing
+}
+func (db *DB) DeleteReminderListing(id string) *model.DeleteReminderResponse {
+
+	return &model.DeleteReminderResponse{DeleteReminderID: id}
 }
