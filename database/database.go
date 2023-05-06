9606af0a6cb0d6fc9ddca12d5fd1ecda4f652c6f
furkanadiiguzel <furkan.adiguzel@ug.bilkent.edu.tr>
2023-05-06 17:55:52 +0300

    database file fixed

--- database/database.go
+++ database/database.go
@@ -1,36 +1,28 @@
 package database
 
 import (
-	"context"
 	"database/sql"
 	"fmt"
-	"time"
 
 	"github.com/furkanadiiguzel/graphql-reminder/graph/model"
+	_ "github.com/lib/pq"
 )
-
-var connectionString = "postgres://postgres:1234@localhost:5432/dbname?sslmode=require"
 
 type DB struct {
 	client *sql.DB
 }
 
-func Connect() (*DB, error) {
+func Connect() *DB {
+	connectionString := "postgres://username:password@furkanadiguzel:5432/dbname?sslmode=require"
 	db, err := sql.Open("postgres", connectionString)
 	if err != nil {
-		return nil, fmt.Errorf("failed to connect to database: %w", err)
+		fmt.Println("Error opening database connection:", err)
+		return nil
 	}
+	defer db.Close()
+	return &DB{client: db}
 
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-
-	if err = db.PingContext(ctx); err != nil {
-		return nil, fmt.Errorf("failed to ping database: %w", err)
-	}
-
-	return &DB{client: db}, nil
 }
-
 func (db *DB) GetReminder(id string) *model.ReminderListing {
 	var reminderListing model.ReminderListing
 	return &reminderListing
--- go.mod
+++ go.mod
@@ -12,6 +12,7 @@ require (
 	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
 	github.com/hashicorp/golang-lru/v2 v2.0.1 // indirect
+	github.com/lib/pq v1.10.9 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/urfave/cli/v2 v2.24.4 // indirect
